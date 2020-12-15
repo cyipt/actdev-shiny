a5d23c0cecca258b353826ffa4b805f23fecfdcf
Robin Lovelace <rob00x@gmail.com>
2020-12-15 15:47:47 +0000

    Add installation instructions

--- app.R
+++ app.R
@@ -3,6 +3,7 @@
 
 remotes::install_github("itsleeds/jts")
 remotes::install_github("robinlovelace/ukboundaries")
+devtools::install_github("RinteRface/shinydashboardPlus")
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
